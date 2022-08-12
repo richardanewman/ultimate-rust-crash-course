f38fbd4a0bcd6d5d65e45fb6c0d53ad897bc7c73
Rick Newman <rick@richardnewman.dev>
2022-08-11 22:05:27 -0400

    control flow exercise

--- src/exercise/mod.rs
+++ src/exercise/mod.rs
@@ -1,3 +1,4 @@
+pub mod control_flow_strings;
 pub mod functions;
 pub mod simple_types;
 pub mod variables;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod exercise;
 
+use exercise::control_flow_strings::run_control_flow_exercise;
 use exercise::functions::run_function_exercise;
 use exercise::simple_types::run_simple_types_exercise;
 use exercise::variables::run_variables_exercise;
@@ -8,4 +9,5 @@ fn main() {
     run_variables_exercise();
     run_function_exercise();
     run_simple_types_exercise();
+    run_control_flow_exercise();
 }
